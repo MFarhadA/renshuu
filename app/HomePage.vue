bba586c8a86bbfb7d3078c3f50228efb6f25eaca
iw-ajilla <312760819+iw-ajilla@users.noreply.github.com>
2026-08-21 09:51:56 +0700

    add HomePage component and update app.vue to use it

---
+++ app/HomePage.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h3>Welcome to Nuxt 3</h3>
+    <p>
+      This is a minimal Nuxt 3 project. You can start by editing the
+      <code>app/HomePage.vue</code> file.
+    </p>
+  </div>
+</template>
--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,10 @@
+<script setup lang="ts">
+import HomePage from './HomePage.vue';
+
+</script>
+
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <HomePage />
   </div>
 </template>
