e82925145d712509733f3c252bc864d96cc4a885
MFarhadA <135182880+MFarhadA@users.noreply.github.com>
2026-08-21 09:57:53 +0700

    refactor HomePage.vue to implement counter functionality with increment and decrement buttons

--- app/HomePage.vue
+++ app/HomePage.vue
@@ -1,9 +1,20 @@
+<script setup lang="ts">
+  const count = ref(0);
+
+  function increment() {
+    count.value++;
+  }
+
+  function decrement() {
+    count.value--;
+  }
+</script>
+
 <template>
   <div>
     <h3>Welcome to Nuxt 3</h3>
-    <p>
-      This is a minimal Nuxt 3 project. You can start by editing the
-      <code>app/HomePage.vue</code> file hehe.
-    </p>
+      <p>Count: {{ count }}</p>
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="increment">Increment</button>
+      <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" @click="decrement">Decrement</button>
   </div>
 </template>
